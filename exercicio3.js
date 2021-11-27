f5d9dc87ff74aa8c5b1b974e36e5a3085eda2666
Heitor Amaral Santos <heitor.amaral@prodap.com.br>
2021-11-26 22:38:33 -0300

    Exercicio 03 feito

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,18 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const paragrafos = document.querySelectorAll('.botao-expandir-retrair')
+
+paragrafos.forEach((p) => {
+    p.addEventListener('click', (e) => {
+        const elementoPai = e.currentTarget.parentNode
+        const elementoAtul = e.currentTarget
+
+        if(elementoAtul.textContent == '+') {
+            elementoAtul.textContent = '-'
+        } else {
+            elementoAtul.textContent = '+'
+        }
+        elementoPai.classList.toggle("expandido") 
+    })
+})
